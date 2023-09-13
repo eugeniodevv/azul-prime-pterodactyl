5f8da1368831108755e6e1c566a12d18327e710b
Eugenio S. <eugeniodevpersonal@gmail.com>
2023-09-14 00:59:20 +0200

    Rework

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,4 +46,4 @@ PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat
 # from the container itself.
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
 # shellcheck disable=SC2086
-eval ${PARSED}
+exec eval ${PARSED}
